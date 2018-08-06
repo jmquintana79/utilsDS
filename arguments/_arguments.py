4e7c7f53c8763e6f9359c2e1005b02890fc4915a
Juan Quintana <juan.armaguedon@gmail.com>
2018-08-06 17:14:27 +0900

    fix the arguments file

--- arguments/_arguments.py
+++ arguments/_arguments.py
@@ -2,8 +2,4 @@
 # @Author: Juan Quintana
 # @Date:   2018-08-06 13:49:31
 # @Last Modified by:   Juan Quintana
-# @Last Modified time: 2018-08-06 17:02:18
-import os
-
-folder_proj = '/home/juan/Workspace/Development/GIT/tools_mlproj/src'
-folder_data_raw = os.path.join(folder_proj, 'data/raw')
+# @Last Modified time: 2018-08-06 17:13:37
--- logs/log-development.md
+++ logs/log-development.md
@@ -1,5 +1,5 @@
 # LOG: Development
 
-###[2018-08-06]:
+### [2018-08-06]:
 - Check a scikit-learn dataset (attributes) and download a sample into csv format.
 - Reader: general function to read csv file to be stored into a pandas df.
