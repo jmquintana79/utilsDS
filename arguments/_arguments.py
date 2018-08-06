c5d4a854600cae89bd3ff31572b80cc607819f64
Juan Quintana <juan.armaguedon@gmail.com>
2018-08-06 17:38:50 +0900

    Update _arguments.py

--- arguments/_arguments.py
+++ arguments/_arguments.py
@@ -3,8 +3,3 @@
 # @Date:   2018-08-06 13:49:31
 # @Last Modified by:   Juan Quintana
 # @Last Modified time: 2018-08-06 17:36:29
-
-import os
-
-folder_proj = '/home/juan/Workspace/Development/GIT/tools_mlproj/src'
-folder_data_raw = os.path.join(folder_proj, 'load_iris.csv')
