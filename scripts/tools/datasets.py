0da0af5cb353a0ea7b413288999ba8511d2a4914
Juan Quintana <juan.armaguedon@gmail.com>
2018-08-30 17:48:35 +0900

    small modification in dataset

--- scripts/tools/datasets.py
+++ scripts/tools/datasets.py
@@ -1,26 +1,27 @@
 # -*- coding: utf-8 -*-
 # @Author: juan
 # @Date:   2018-08-27 21:56:15
-# @Last Modified by:   juan
-# @Last Modified time: 2018-08-27 22:58:35
+# @Last Modified by:   Juan Quintana
+# @Last Modified time: 2018-08-30 16:37:25
 
 from pandas import DataFrame
+
 
 class Data():
     def __init__(self):
         self.menu = ['boston', 'iris', 'diabetes']
 
-    def load(self,sname:str):
+    def load(self, sname: str):
         # cases
-        if sname=='boston':
+        if sname == 'boston':
             ## boston (regression)
             from sklearn.datasets import load_boston
             dataset = load_boston()
-        elif sname=='iris':
+        elif sname == 'iris':
             ## iris (classification)
             from sklearn.datasets import load_iris
             dataset = load_iris()
-        elif sname=='diabetes':
+        elif sname == 'diabetes':
             ## diabetes (regression)
             from sklearn.datasets import load_diabetes
             dataset = load_diabetes()
@@ -29,10 +30,10 @@ class Data():
             return None
 
         # display shape
-        print(dataset.data.shape)
+        print('[info] shape of loaded data: ', dataset.data.shape)
 
         # store data in df
-        dfdata = DataFrame(dataset.data,columns=dataset.feature_names)
+        dfdata = DataFrame(dataset.data, columns=dataset.feature_names)
         dfdata['target'] = dataset.target
         lcol = list(dfdata.columns)
 
@@ -54,12 +55,13 @@ class Data():
         }
 
         # return
-        return (dfdata,dcol)
+        return (dfdata, dcol)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     # object
     dataset = Data()
-    print('Available dataset: %s'%dataset.menu)
+    print('Available dataset: %s' % dataset.menu)
     # load dataset
     print('Load boston dataset:')
     data, dcol = dataset.load('boston')
