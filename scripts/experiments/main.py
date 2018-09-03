ba15c4819ca5cd58dbda3d11092763b5f6321bb8
Juan Quintana <juan.armaguedon@gmail.com>
2018-09-03 17:30:37 +0900

    add 'mlflow.start_run()'

--- scripts/experiments/main.py
+++ scripts/experiments/main.py
@@ -2,7 +2,7 @@
 # @Author: Juan Quintana
 # @Date:   2018-09-03 15:40:43
 # @Last Modified by:   Juan Quintana
-# @Last Modified time: 2018-09-03 17:21:15
+# @Last Modified time: 2018-09-03 17:28:28
 
 """
 RUN EXPERIMENTS using MLFLOW.
@@ -43,25 +43,25 @@ def main(exp_num, alpha, l1_ratio):
     click.secho('[arg] alpha = %s' % alpha, fg='green')
     click.secho('[arg] l1_ratio = %s' % l1_ratio, fg='green')
 
-    # with mlflow.start_run():
-    # # model
-    from experiments import model
-    test_y, test_yhat, k = model.launcher(dfdata, alpha, l1_ratio)
+    with mlflow.start_run():
+        # # model
+        from experiments import model
+        test_y, test_yhat, k = model.launcher(dfdata, alpha, l1_ratio)
 
-    # # scoring
-    dmetrics = metrics_regression(test_y, test_yhat, k)
-    # print metrics
-    click.echo("[info] Metrics: ")
-    click.secho("  BIAS: %s" % dmetrics['bias'], fg='red')
-    click.secho("  MAE: %s" % dmetrics['mae'], fg='red')
-    click.secho("  R2: %s" % dmetrics['r2'], fg='red')
+        # # scoring
+        dmetrics = metrics_regression(test_y, test_yhat, k)
+        # print metrics
+        click.echo("[info] Metrics: ")
+        click.secho("  BIAS: %s" % dmetrics['bias'], fg='red')
+        click.secho("  MAE: %s" % dmetrics['mae'], fg='red')
+        click.secho("  R2: %s" % dmetrics['r2'], fg='red')
 
-    # tracking
-    mlflow.log_param("alpha", alpha)
-    mlflow.log_param("l1_ratio", l1_ratio)
-    mlflow.log_metric("bias", dmetrics['bias'])
-    mlflow.log_metric("mae", dmetrics['mae'])
-    mlflow.log_metric("r2", dmetrics['r2'])
+        # tracking
+        mlflow.log_param("alpha", alpha)
+        mlflow.log_param("l1_ratio", l1_ratio)
+        mlflow.log_metric("bias", dmetrics['bias'])
+        mlflow.log_metric("mae", dmetrics['mae'])
+        mlflow.log_metric("r2", dmetrics['r2'])
 
 
 if __name__ == '__main__':
