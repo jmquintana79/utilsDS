86e1ee3447883513bbc764eb73ab9dd448385c2f
Juan Quintana <juan.armaguedon@gmail.com>
2018-09-25 18:11:32 +0900

    included a outliers cleaner in the tuning script for testing

--- scripts/models/tuning.py
+++ scripts/models/tuning.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
 # @Author: jmquintana79
 # @Date:   2018-09-22 11:58:53
-# @Last Modified by:   jmquintana79
-# @Last Modified time: 2018-09-23 00:46:44
-
-
-
+# @Last Modified by:   Juan Quintana
+# @Last Modified time: 2018-09-25 18:02:16
 
 
 if __name__ == '__main__':
@@ -34,37 +31,44 @@ if __name__ == '__main__':
     data, dcol = solar.load()
     # select data
     ly = ['y']
-    lx = ['doy', 'hour','LCDC267', 'MCDC267', 'HCDC267', 'TCDC267', 'logAPCP267', 'RH267', 'TMP267', 'DSWRF267']
+    lx = ['doy', 'hour', 'LCDC267', 'MCDC267', 'HCDC267', 'TCDC267', 'logAPCP267', 'RH267', 'TMP267', 'DSWRF267']
     data = data[lx + ly]
     fdcol = get_dcol(data, ltarget=ly)
     # select one hour data
     hour = 11
-    idata = data[data.hour==hour]
-    idata.drop('hour', axis =1, inplace = True)
-    idcol = get_dcol(idata, ltarget = ['y'])
+    idata = data[data.hour == hour]
+    idata.drop('hour', axis=1, inplace=True)
+    idcol = get_dcol(idata, ltarget=['y'])
     # clean
     del(data)
     del(dcol)
+
+    # filtering outliers (ghi vs power)
+    from preprocessing.outliers import median2D
+    isoutlier = median2D.launch(idata['DSWRF267'].values, idata.y.values, percent=20.)
+    idata['isoutlier'] = isoutlier
+    idata = idata[idata.isoutlier == False]
+    idata.drop('isoutlier', axis=1, inplace=True)
+
     # prepare data
     X = idata[idcol['lx']].values
     scaler = Scaler()
     y = scaler.fit_transform(idata[idcol['ly']].values).ravel()
-    print('Prepared data: X: %s  y: %s'%(X.shape, y.shape))
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3,random_state=42)
-    print('Prepared data: X_train: %s  y_train: %s'%(X_train.shape, y_train.shape))
-    print('Prepared data: X_test: %s  y_test: %s'%(X_test.shape, y_test.shape))
+    print('Prepared data: X: %s  y: %s' % (X.shape, y.shape))
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+    print('Prepared data: X_train: %s  y_train: %s' % (X_train.shape, y_train.shape))
+    print('Prepared data: X_test: %s  y_test: %s' % (X_test.shape, y_test.shape))
 
-    ## estimator withou tuning
+    # estimator withou tuning
     t.add('no_tuning')
     clf = XGBRegressor(nthreads=-1)
     clf.fit(X_train, y_train)
     y_hat = clf.predict(X_test)
     dscores = metrics_regression(y_test, y_hat, X.shape[1])
     tf = t.since('no_tuning')
-    print('Without tuning:     bias = %.3f  mae = %.3f  r2 = %.3f (time: %s)'%( dscores['bias'], dscores['mae'], dscores['r2'], format_duration(tf)))
+    print('Without tuning:     bias = %.3f  mae = %.3f  r2 = %.3f (time: %s)' % (dscores['bias'], dscores['mae'], dscores['r2'], format_duration(tf)))
 
-
-    ## extimator with random tuning
+    # extimator with random tuning
     t.add('random_tuning')
     clf = XGBRegressor(nthreads=-1)
     one_to_left = st.beta(10, 1)
@@ -79,26 +83,25 @@ if __name__ == '__main__':
         'reg_alpha': from_zero_positive,
         "min_child_weight": from_zero_positive,
     }
-    gs = RandomizedSearchCV(clf, dparams, cv = 5, n_jobs = 1, scoring = 'r2')
+    gs = RandomizedSearchCV(clf, dparams, cv=5, n_jobs=1, scoring='r2')
     gs.fit(X_train, y_train)
     y_hat = gs.best_estimator_.predict(X_test)
     dscores = metrics_regression(y_test, y_hat, X.shape[1])
     tf = t.since('random_tuning')
-    print('Random tuning:      bias = %.3f  mae = %.3f  r2 = %.3f (time: %s)'%( dscores['bias'], dscores['mae'], dscores['r2'], format_duration(tf)))
+    print('Random tuning:      bias = %.3f  mae = %.3f  r2 = %.3f (time: %s)' % (dscores['bias'], dscores['mae'], dscores['r2'], format_duration(tf)))
 
-
-    ## extimator with exhaustive tuning
+    # extimator with exhaustive tuning
     t.add('exhaustive_tuning')
     clf = XGBRegressor(nthreads=-1)
     dparams = {
         "n_estimators": [3, 10, 25, 40],
         "max_depth": [3, 10, 25, 40],
         "learning_rate": [0.05, 0.1, 0.25, 0.5],
-        "gamma": np.arange(0,11,1),
+        "gamma": np.arange(0, 11, 1),
     }
-    gs = GridSearchCV(clf, param_grid=dparams, cv = 5, n_jobs = 1, scoring = 'r2')
+    gs = GridSearchCV(clf, param_grid=dparams, cv=5, n_jobs=1, scoring='r2')
     gs.fit(X_train, y_train)
     y_hat = gs.best_estimator_.predict(X_test)
     dscores = metrics_regression(y_test, y_hat, X.shape[1])
     tf = t.since('exhaustive_tuning')
-    print('Exhaustive tuning:  bias = %.3f  mae = %.3f  r2 = %.3f (time: %s)'%( dscores['bias'], dscores['mae'], dscores['r2'], format_duration(tf)))
+    print('Exhaustive tuning:  bias = %.3f  mae = %.3f  r2 = %.3f (time: %s)' % (dscores['bias'], dscores['mae'], dscores['r2'], format_duration(tf)))
