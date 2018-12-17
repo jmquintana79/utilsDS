2a889e5e29fcc1e83a5090a6edecb63c23380bb3
Juan Quintana <juan.armaguedon@gmail.com>
2018-12-17 14:56:12 +0900

    new structure of this repository

--- Makefile
+++
@@ -1,5 +0,0 @@
-clean:
-	rm -f data/raw/*.csv
-create:
-	mkdir -p data/raw data/intermediate data/external data/processed
-
--- scripts/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Author: jmquintana79
-# @Date:   2018-08-30 23:53:16
-# @Last Modified by:   jmquintana79
-# @Last Modified time: 2018-08-30 23:54:01
