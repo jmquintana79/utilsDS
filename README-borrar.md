8f8c5eee751ba1d16af0b8dc9e378260eaa871cd
Juan <juan.armaguedon@icloud.com>
2021-11-20 16:58:33 +0100

    first draft of a readme file builder

---
+++ README-borrar.md
@@ -0,0 +1,66 @@
+# Tools for a Data Science projects
+
+This repository is a compendium of notebooks and scripts to be used in my daily work for Data Science projects.
+
+These tools have been written into Python programming languages. The necessary libraries are included in [environment.yml](https://github.com/jmquintana79/tools_mlproj/blob/master/environment.yml) to be installed using Anaconda.
+
+## Content
+### Tools
+- Data understanding *[0]*
+- Experiments mlflow *[5]*
+### Scripts
+- Analysis *[0]*
+      - Ada *[5]*
+      - Errors *[3]*
+- Plot *[4]*
+- Datasets *[8]*
+      - Tools *[1]*
+      - Kaggle *[0]*
+         - Titanic *[1]*
+      - Summaries *[0]*
+      - Data *[0]*
+- Pipelines *[3]*
+- Utils *[6]*
+- Models *[7]*
+      - Xgboost *[5]*
+      - Gam *[1]*
+- Preprocessing *[2]*
+      - Scalers *[1]*
+      - Smoothers *[1]*
+      - Outliers *[3]*
+- Data *[2]*
+### Notebooks
+- Statistics *[0]*
+      - Sampling *[3]*
+      - Hypotesis testing *[1]*
+      - Distributions *[1]*
+- Analysis *[0]*
+      - Scatter plots *[1]*
+      - Eda *[4]*
+      - Fourier *[2]*
+      - Missing *[1]*
+      - Kde *[1]*
+      - Autoeda *[4]*
+      - Bar plots *[1]*
+      - Errors *[1]*
+- Pandas accelerators *[1]*
+- Datasets *[4]*
+- Similarity *[1]*
+- Pipelines *[1]*
+- Automl *[1]*
+      - Extended eda *[0]*
+      - Automl 1 *[0]*
+         - Ensemble *[0]*
+         - 6 default randomforest *[0]*
+         - Eda *[0]*
+         - 2 decisiontree *[0]*
+         - 3 linear *[0]*
+         - 1 baseline *[0]*
+         - 5 default neuralnetwork *[0]*
+         - 4 default xgboost *[0]*
+- NLP *[9]*
+- Feature engineering *[0]*
+      - Selection *[1]*
+- Preprocessing *[0]*
+      - Anomalies *[2]*
+> Updated on 2021-11-20 16:56:28.283897.
---
+++ build_readme.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python
+
+import os
+from datetime import datetime
+
+def display(path, root):
+    print((len(path) - 2) * '---', os.path.basename(root), f'[directory]')
+
+def intersection(lst1, lst2):
+    return list(set(lst1) & set(lst2))
+
+def count_files(files:list)->int:
+    return len([file for file in files if 'py' in file or '.ipynb' in file])
+
+
+# initialize
+omossions_root = ['.', '.ipynb_checkpoints']
+omossions_in_path = ['.git']
+records = list()
+stab = '   '
+url_root = "https://github.com/jmquintana79/utilsDS/blob/master"
+
+# header
+records.append('# Tools for a Data Science projects')
+
+# introduction
+s_intro = """
+This repository is a compendium of notebooks and scripts to be used in my daily work for Data Science projects.
+
+These tools have been written into Python programming languages. The necessary libraries are included in [environment.yml](https://github.com/jmquintana79/tools_mlproj/blob/master/environment.yml) to be installed using Anaconda.
+"""
+records.append(s_intro)
+
+# add content title
+records.append('## Content')
+
+# traverse root directory, and list directories as dirs and files as files
+for root, dirs, files in os.walk("."):
+    # get path (splitted root)
+    path = root.split(os.sep)
+    # build name
+    name = os.path.basename(root)
+    if name == 'nlp':
+        s_name = name.upper()
+    else:
+        s_name = f"{name.replace('_', ' ').capitalize() }"
+    # build counter files
+    nfiles = count_files(files)
+    s_nfiles = f'*[{count_files(files)}]*'
+    # number of tabs
+    ntabs = len(path)-2
+    # build url
+    url = os.path.join(url_root, root[2:])
+    # omit folders in path
+    if len(intersection(path, omossions_in_path)) > 0:
+        pass
+    else:
+        # omit folders
+        if not os.path.basename(root) in omossions_root:
+            # display
+            display(path, root)
+            # add sub-headers
+            if ntabs == 0:
+                records.append(f'### {s_name}')
+            elif ntabs == 1:
+                records.append(f'- {s_name} *[{nfiles}]*')
+            else:
+                records.append(f'{ntabs*stab}- {s_name} {s_nfiles}')
+
+
+# foot
+s_foot = f'> Updated on {datetime.now()}.'
+records.append(s_foot)
+
+## WRITE FILE
+with open('README-borrar.md', 'w') as outfile:
+    # loop of records
+    for r in records:
+        outfile.write(f'{r}\n')
+
+
+# end
+quit('done')
