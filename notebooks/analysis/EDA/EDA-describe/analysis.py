44dfdb15171bb679c4bf622da3ea708d3eb511f2
Juan <juan.armaguedon@icloud.com>
2022-01-30 00:33:24 +0100

    new feature

--- notebooks/analysis/EDA/EDA-describe/analysis.py
+++ notebooks/analysis/EDA/EDA-describe/analysis.py
@@ -17,17 +17,22 @@ def info(data:pd.DataFrame, decimals:int = 2)->pd.DataFrame:
     # copy data
     df = data.copy()
     # get names of numeric columns
-    cols_num = df.select_dtypes(include=['float64']).columns.values
+    cols_num = df.select_dtypes(include=['float64', 'int64']).columns.values
     # get names of categorical columns
-    cols_cat = df.select_dtypes(include=['object', 'int64', 'category', 'bool', 'datetime64[ns]']).columns.values
+    cols_cat = df.select_dtypes(include=['object', 'category', 'bool', 'datetime64[ns]']).columns.values
     # get types information
     dfinfo = pd.DataFrame({'variable':df.dtypes.index, 'types':df.dtypes.values}).set_index('variable')
     # dtype to str
     dfinfo['types'] = dfinfo['types'].astype(str)
-    # estimate number of unique values for categorical variables
+    # other types of data in each column
+    dfinfo['mixed_types'] = np.ones(len(dfinfo)) * np.nan
+    for col in df.columns:
+        dfinfo.loc[col,'mixed_types'] = ','.join([str(x).replace('<class','').replace('>','') for x in df[col].dropna().apply(type).unique()])
+    # estimate number of unique values for categorical and numerical variables
     dfinfo['unique'] = np.ones(len(dfinfo)) * np.nan
-    for col in cols_cat:
+    for col in df.columns:
         dfinfo.loc[col,'unique'] = len(df[col].unique())
+    dfinfo['unique'] = dfinfo['unique'].astype(int)
     # estimate order of magnitude for numerical variables
     dfinfo['magnitude'] = np.ones(len(dfinfo)) * np.nan
     for col in cols_num:
